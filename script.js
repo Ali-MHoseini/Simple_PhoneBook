d7175b336f36766e45f93a1a5605e728f43369ec
Ali.MHoseini <ali.mhoseini99@gmail.com>
2022-12-10 14:22:53 +0330

    add search and sorting

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ const contactName = document.getElementById('contactName'),
     addContact = document.getElementById('addContact'),
     contacts__list = document.getElementById('contacts__list'),
     searchName = document.getElementById('searchName'),
-    searchModel = document.getElementById('searchModel');
+    searchModel = document.getElementById('searchModel'),
+    telbox = document.getElementById('telbox');
 
+window.deleteItem = deleteItem;
+
+const mobileRegex = /^(?:0|98|\+98|\+980|0098|098|00980)?(9\d{9})$/
+const phoneRegex = /^0[0-9]{2,}[0-9]{7,}$/
+
+let isTrueReg = false;
 
 const contact = {
     getItem() {
-        const datas = JSON.parse(localStorage.getItem("contacts"))
-        console.log(datas);
-        return datas;
+        return JSON.parse(localStorage.getItem("contacts"))??[];
 
     },
     setItem(newUserData) {
@@ -25,42 +30,36 @@ const contact = {
             name,
             number,
             modNumber,
-            id: Date.now().toString(36)
+            id: Math.floor(Math.random() *10000)
         }
 
         this.setItem(newContact);
     },
 
-    searchItem() {
-        const searchExp = searchModel.value.toLowerCase();
-        const matchedData = this.getItem().filter(value => value.name.toLowerCase().search(searchExp) >= 0)
-        showTable(matchedData);
-
-    },
     removeItem(id) {
         const data = this.getItem();
         const newData = data && data.filter(item => item.id !== id)
         localStorage.setItem("contacts", JSON.stringify(newData))
-        showList()
+        showList(newData)
     },
-    editItem(id, newText) {
+    editItem(id, newName,newNumber) {
         const data = this.getData()
         const newData = data.map(item => {
             if (item.id === id) {
-                return {...item, text: newText }
+                return {...item, name: newName, number:newNumber }
             } else {
                 return {...item }
             }
         })
 
         localStorage.setItem("contacts", JSON.stringify(newData))
-        showList()
+        showList(newData)
     }
 }
-const showList = () => {
+const showList = (data) => {
     contacts__list.innerHTML = "";
 
-    const contactData = contact.getItem();
+    const contactData = data
 
     if (contactData.lenght === 0) {
         contacts__list.innerText = 'در حال حاضر نامی وجود ندارد...';
@@ -74,7 +73,7 @@ const showList = () => {
             <p>${item.number}</p>
             <p>${item.modNumber}</p>
             <div>
-                <button id="delete" class="contact_btn">حذف</button>
+                <button id="delete" class="contact_btn" onclick="deleteItem(${item.id})">حذف</button>
                 <button id="edit" class="contact_btn">ویرایش</button>
             </div>
         </li>`
@@ -83,6 +82,59 @@ const showList = () => {
     }
 }
 
+contactTel.addEventListener('input', function() {
+    if(phoneModel.value ==='1') {
+        if (mobileRegex.test(contactTel.value)){
+            telbox.style.border = '2px solid green';
+            isTrueReg = true;
+        }
+        else {
+            telbox.style.border = '2px solid red';
+            isTrueReg = false;
+        }
+    }else {
+        if (phoneRegex.test(contactTel.value)){
+            telbox.style.border = '2px solid green';
+            isTrueReg = true;
+        }
+        else if(!phoneRegex.test(contactTel.value)) {
+            telbox.style.border = '2px solid red';
+            isTrueReg = false;
+        }
+    }
+})
+
 addContact.addEventListener('click', function() {
-    contact.addItem(contactName.value, contactTel.value, phoneModel.value);
+    if (isTrueReg) {
+        contact.addItem(contactName.value, contactTel.value, phoneModel.value);
+    } else {
+        alert('لطفا شماره درست را وارد بفرمایید')
+    }
+    contactName.innerText = "";
+    contactTel.innerText = "";
+})
+
+function deleteItem (id) {
+    contact.removeItem(id);
+}
+
+searchName.addEventListener('input', () => {
+    const searchExp = searchName.value.toLowerCase();
+    const matchedData = contact.getItem().filter(value => value.name.toLowerCase().search(searchExp) >= 0)
+    showList(matchedData);
+})
+
+searchModel.addEventListener('input',() => {
+    const searchExp = searchModel.value;
+    if (searchExp == '1') {
+        const matchedData = contact.getItem().filter(value => value.modNumber == 1)
+        console.log(matchedData)
+        showList(matchedData);
+    }else if(searchExp == '2') {
+        const matchedData = contact.getItem().filter(value => value.modNumber == 2)
+        showList(matchedData);
+    }else {
+        showList(contact.getItem())
+    }
+
 })
